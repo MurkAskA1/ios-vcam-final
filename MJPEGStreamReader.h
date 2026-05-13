ce4e78df0fb0e0ae234cfea7574bd1e80c2162f5
MurkAskA1 <AlisonPer1@proton.me>
2026-05-13 10:35:37 -0400

    Add MJPEGStreamReader header.

---
+++ MJPEGStreamReader.h
@@ -0,0 +1,22 @@
+// MJPEGStreamReader.h
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void (^MJPEGFrameCallback)(UIImage *frame);
+typedef void (^MJPEGErrorCallback)(NSError *error);
+
+@interface MJPEGStreamReader : NSObject <NSURLSessionDataDelegate>
+@property (nonatomic, strong, readonly) NSURL *streamURL;
+@property (nonatomic, assign, readonly) BOOL isConnecting;
+@property (nonatomic, assign, readonly) NSUInteger frameCount;
+@property (nonatomic, copy, nullable) MJPEGFrameCallback frameCallback;
+@property (nonatomic, copy, nullable) MJPEGErrorCallback errorCallback;
+
+- (instancetype)initWithURL:(NSURL *)url;
+- (void)startStreaming;
+- (void)stopStreaming;
+@end
+
+NS_ASSUME_NONNULL_END
